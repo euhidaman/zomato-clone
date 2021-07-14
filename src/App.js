f8316b95f85ae5903209fd919c368e548cb129f1
Euhid Aman <aman.derax20@gmail.com>
2021-07-14 19:50:39 +0530

    Added food-options images on second header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
 import Food from './components/Food';
+import FoodOptions from './components/FoodOptions';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <FoodOptions />
       <Food />
     </div>
   );
---
+++ src/components/FoodOptions.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const FoodOptions = () => {
+    return (
+        <div className="flex mt-10 justify-around px-20" >
+
+            <div className="flex">
+                <button class="bg-pink-50 hover:bg-pink-100 mx-5 py-3 px-3 rounded-full">
+                    <img className="w-10 h-10 bg-pink-100"
+                        src="https://b.zmtcdn.com/data/o2_assets/c0bb85d3a6347b2ec070a8db694588261616149578.png?output-format=webp"
+                        alt="delivery" />
+                </button>
+                <h1 className="mt-3.5 font-okra text-xl text-pink-600">Delivery</h1>
+            </div>
+
+            <div className="flex">
+                <button class="bg-gray-50 hover:bg-pink-100 mx-5 py-3 px-3 rounded-full">
+                    <img className="w-10 h-10 bg-gray-100 hover:bg-pink-100"
+                        src="https://b.zmtcdn.com/data/o2_assets/78d25215ff4c1299578ed36eefd5f39d1616149985.png?output-format=webp"
+                        alt="delivery" />
+                </button>
+                <h1 className="mt-3.5 font-okra text-xl text-gray-500">Dining Out</h1>
+            </div>
+
+            <div className="flex">
+                <button class="bg-gray-50 hover:bg-pink-100 mx-5 py-3 px-3 rounded-full">
+                    <img className="w-10 h-10 bg-gray-100 hover:bg-pink-100"
+                        src="https://b.zmtcdn.com/data/o2_assets/01040767e4943c398e38e3592bb1ba8a1616150142.png"
+                        alt="delivery" />
+                </button>
+                <h1 className="mt-3.5 font-okra text-xl text-gray-500">Nightlife</h1>
+            </div>
+
+            <div className="flex">
+                <button class="bg-gray-50 hover:bg-pink-100 mx-5 py-3 px-3 rounded-full">
+                    <img className="w-10 h-10 bg-gray-100 hover:bg-pink-100"
+                        src="https://b.zmtcdn.com/data/o2_assets/54cad8274d3c3ec7129e0808a13b27c31616582882.png"
+                        alt="delivery" />
+                </button>
+                <h1 className="mt-3.5 font-okra text-xl text-gray-500">Nutrition</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default FoodOptions
