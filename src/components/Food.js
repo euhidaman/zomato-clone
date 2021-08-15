242d8d2414aa96721ef9b5bddf3144de9892d9fc
Euhid Aman <aman.derax20@gmail.com>
2021-08-15 14:48:01 +0530

    changed title of website

--- src/components/Food.js
+++ src/components/Food.js
@@ -20,15 +20,14 @@ const Food = () => {
         <div className="text-gray-400  body-font w-full">
             <div className="container px-10 py-10 mx-auto">
                 <div className="flex flex-wrap -m-4 pl-6">
-                    {/* image-first */}
 
                     {resDetail.map((food) => (
-                        <div className="mt-2 p-2 lg:w-1/4 md:w-1/2 transform transition duration-200 rounded-lg hover:scale-105 hover:shadow-2xl">
+                        <div className="mb-4 p-2 lg:w-1/4 md:w-1/2 transform transition duration-200 rounded-lg hover:scale-105 hover:shadow-lg">
                             <div className="h-full flex flex-col items-center text-center">
                                 <img alt="team" className="flex-shrink-0 rounded-lg w-full h-60 object-cover object-center mb-2" src={food.hotelUrl} />
                                 <div className="w-full">
-                                    <h2 className="title-font font-medium text-lg text-white">Alper Kamu</h2>
-                                    <h3 className="text-gray-500 mb-3">UI Developer</h3>
+                                    <h2 className="title-font font-medium text-lg text-white -mt-2">Alper Kamu</h2>
+                                    <h3 className="text-gray-500 -mt-5 mb-3">UI Developer</h3>
                                     <p className="">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
                                 </div>
                             </div>
