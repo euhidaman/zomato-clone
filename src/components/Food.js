7265a3c3a8b82146ef2548806dacbf2923ef6b7d
Euhid Aman <aman.derax20@gmail.com>
2021-08-16 10:49:59 +0530

    displayed price with style

--- src/components/Food.js
+++ src/components/Food.js
@@ -34,8 +34,6 @@ const Food = () => {
                             <div className="h-full flex flex-col items-center text-center">
                                 <img alt="team" className="flex-shrink-0 rounded-lg w-full h-60 object-cover object-center mb-2" src={food.hotelUrl} />
 
-
-
                                 <div className="w-full ">
                                     <div className="flex justify-between bg-transparent -mt-10">
                                         <h2 className="text-white bg-blue-500 text-left ">&nbsp; {food.discount} &nbsp;</h2>
@@ -47,15 +45,22 @@ const Food = () => {
 
                                 <div className="w-full">
                                     <div className="flex justify-between">
-                                        <h3 className="text-gray-600 text-left text-lg mb-3">{food.hotelName}</h3>
-                                        <h3 className="text-gray-50 text-left px-2 mb-3 flex bg-green-500 rounded-md">
+                                        <h3 className="text-gray-600 text-left text-lg ">{food.hotelName}</h3>
+                                        <h3 className="text-gray-50 text-left px-2  flex bg-green-500 rounded-md">
                                             {food.hotelRating}
                                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-0.5 mt-0.5 bg-green-500" viewBox="0 0 20 20" fill="currentColor">
                                                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                                             </svg>
                                         </h3>
                                     </div>
-                                    <p className="text-gray-500 text-left text-base mb-3">{food.description}</p>
+
+                                    <div className="w-full">
+                                        <div className="flex justify-between py-1">
+                                            <p className="text-gray-400 text-left text-sm mb-3">{food.hotelType}</p>
+                                            <h3 className="text-gray-400 text-right text-sm">{food.price}</h3>
+                                        </div>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
