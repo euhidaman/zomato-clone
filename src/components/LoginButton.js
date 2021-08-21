131ac5dbcb0b938d964cb668adf39c7ff584bdb2
Euhid Aman <aman.derax20@gmail.com>
2021-08-21 14:18:25 +0530

    Added phone-no input in login modal

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -16,13 +16,13 @@ export default function Modal() {
             {showModal ? (
                 <>
                     <div
-                        className="justify-center bg-opacity-40 bg-gray-300 items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                        className="justify-center bg-opacity-70 bg-gray-900 items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50"
                     >
                         <div className="relative  w-auto rounded-3xl my-6 mx-auto max-w-sm">
                             {/*content*/}
-                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white">
                                 {/*header*/}
-                                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                                <div className="flex items-start justify-between p-5 rounded-3xl">
                                     <h3 className="text-3xl font-semibold text-gray-600">
                                         Log in
                                     </h3>
@@ -39,14 +39,26 @@ export default function Modal() {
                                 </div>
                                 {/*body*/}
                                 <div className="relative p-6 flex-auto">
+                                    <input className="w-full h-11 -mt-1 text-lg border border-gray-300 focus:outline-none rounded-md focus:border-8 focus:border-green-800 "
+                                        type="text" placeholder="Phone number"
+                                    />
+                                    <button
+                                        className="w-full mt-3 bg-gray-300 font-sans
+                                        text-lg py-2 rounded shadow text-white outline-none 
+                                        focus:outline-none ease-linear transition-all duration-150"
+                                        type="button"
+                                    >
+                                        Send OTP
+                                    </button>
                                     <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+
                                         I always felt like I could do anything. That’s the main
                                         thing people are controlled by! Thoughts- their perception
                                         of themselves!
                                     </p>
                                 </div>
                                 {/*footer*/}
-                                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                                <div className="flex items-center justify-end p-6  rounded-3xl">
                                     <button
                                         className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                         type="button"
@@ -65,7 +77,6 @@ export default function Modal() {
                             </div>
                         </div>
                     </div>
-                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
                 </>
             ) : null}
         </>
