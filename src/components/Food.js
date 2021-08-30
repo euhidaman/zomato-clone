f4ed071f93a646cfcf14aa9ab7af5672637af510
Euhid Aman <aman.derax20@gmail.com>
2021-08-30 21:14:37 +0530

    Made modal start from top of webapp

--- src/components/Food.js
+++ src/components/Food.js
@@ -73,13 +73,13 @@ const Food = () => {
             {showModal ? (
                 <>
                     <div
-                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                        className="justify-center -mt-80 bg-opacity-70 bg-gray-900 items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50"
                     >
-                        <div className="relative w-auto my-6 mx-auto max-w-6xl">
+                        <div className="relative w-auto mx-auto max-w-6xl rounded-3xl">
                             {/*content*/}
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                 {/*header*/}
-                                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-3xl">
                                     <h3 className="text-3xl font-semibold">
                                         Modal Title
                                     </h3>
@@ -103,7 +103,7 @@ const Food = () => {
                                     </p>
                                 </div>
                                 {/*footer*/}
-                                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                                <div className="flex items-center rounded-3xl justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
                                     <button
                                         className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                         type="button"
@@ -122,7 +122,6 @@ const Food = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
                 </>
             ) : null}
         </>
