75936f5c12ba61d7fe377d2c876bcaf98998023c
Suraj Prabhu <prabhusuraj103@gmail.com>
2021-10-06 15:14:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import { React, useState } from "react";
-import "./App.css";
-import Header from "./components/Header";
-import Food from "./components/Food";
-import FoodOptions from "./components/FoodOptions";
-import ColoredLine from "./components/ColoredLine";
-import FilterButtons from "./components/FilterButtons";
-import Find from "./components/Find";
+import { React, useState } from 'react'
+import './App.css';
+import Header from './components/Header';
+import Food from './components/Food';
+import FoodOptions from './components/FoodOptions';
+import ColoredLine from './components/ColoredLine';
+import FilterButtons from './components/FilterButtons';
 
 function App() {
+
   const [location, setLocation] = useState("Chennai");
-  const [searchQuery, setQuery] = useState("");
-  const [searched, setSearched] = useState(false);
+
   return (
     <div className="App">
-      <Header
-        functions={[location, setLocation]}
-        setQuery={setQuery}
-        setSearched={setSearched}
-        searched={searched}
-      />
+      <Header functions={[location, setLocation]} />
       <FoodOptions />
       <ColoredLine color="gray" />
       <FilterButtons />
-      {searchQuery && searched ? (
-        <Find searchQuery={searchQuery} />
-      ) : (
-        <>
-          <h1 className="flex justify-start relative sm:mx-14 font-normal text-3xl lg:mx-20">
-            Best Food in {location}
-          </h1>
-          <Food />
-        </>
-      )}
+      <h1 className="mx-44 pt-4 text-3xl font-normal" >Best Food in {location}</h1>
+      <Food />
     </div>
   );
 }
