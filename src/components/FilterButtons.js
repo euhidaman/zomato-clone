29846614a57d7ccf6f58f56e58a798e993ca2fb1
Olga Tkach <tkacholya@gmail.com>
2021-10-06 17:13:21 -0400

    Updated Filter component, made it responsive

--- src/components/FilterButtons.js
+++ src/components/FilterButtons.js
@@ -1,36 +1,150 @@
-import React from 'react'
+import React from "react";
 
 const FilterButtons = () => {
-    return (
-        <div class="flex justify-start items-center mx-40 my-3">
-            <div class="flex">
-                <button type="button" class="flex bg-white hover:bg-gray-100 text-gray-400 px-6 py-2 rounded font-medium mx-3 border border-gray-300 transition duration-200 each-in-out">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:bg-gray-100" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
-                    </svg>
-                    Filters
-                </button>
-                <button type="button" class="flex bg-white hover:bg-gray-100 text-gray-400 px-6 py-2 rounded font-medium mx-3 border border-gray-300 transition duration-200 each-in-out">
-                    Rating: 4.0+
-                </button>
-                <button type="button" class="flex bg-white hover:bg-gray-100 text-gray-400 px-6 py-2 rounded font-medium mx-3 border border-gray-300 transition duration-200 each-in-out">
-                    Safe and Hygenic
-                </button>
-                <button type="button" class="flex bg-white hover:bg-gray-100 text-gray-400 px-6 py-2 rounded font-medium mx-3 border border-gray-300 transition duration-200 each-in-out">
-                    Pure Veg
-                </button>
-                <button type="button" class="flex bg-white hover:bg-gray-100 text-gray-400 px-6 py-2 rounded font-medium mx-3 border border-gray-300 transition duration-200 each-in-out">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:bg-gray-100" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4" />
-                    </svg>
-                    Delivery Time
-                </button>
-                <button type="button" class="flex bg-white hover:bg-gray-100 text-gray-400 px-6 py-2 rounded font-medium mx-3 border border-gray-300 transition duration-200 each-in-out">
-                    Great Offers
-                </button>
-            </div>
+  return (
+    <div className="container px-10 md:mx-auto">
+      <div className="flex flex-wrap pt-4">
+        <div className="flex-shrink-0 mr-5 mb-4">
+          <div className="group hover:bg-gray-100 border border-gray-400 text-gray-400 px-4 py-2 rounded-xl flex items-center cursor-pointer h-full">
+            <button type="button" className="flex">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 group-hover:bg-gray-100"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+                />
+              </svg>
+              Filters
+            </button>
+          </div>
         </div>
-    )
-}
+        <div className="flex-shrink-0 mr-5 mb-4">
+          <div className="group hover:bg-gray-100 border border-gray-400 text-gray-400 px-4 py-2 rounded-xl flex items-center cursor-pointer h-full">
+            <button type="button" className="flex">
+              Rating: 4.0+
+            </button>
+          </div>
+        </div>
+        <div className="flex-shrink-0 mr-5 mb-4">
+          <div className="group hover:bg-gray-100 border border-gray-400 text-gray-400 px-4 py-2 rounded-xl flex items-center cursor-pointer h-full">
+            <button type="button" className="flex">
+              Safe and Hygenic
+            </button>
+          </div>
+        </div>
+        <div className="flex-shrink-0 mr-5 mb-4">
+          <div className="group hover:bg-gray-100 border border-gray-400 text-gray-400 px-4 py-2 rounded-xl flex items-center cursor-pointer h-full">
+            <button type="button" className="flex">
+              Pure Veg
+            </button>
+          </div>
+        </div>
+        <div className="flex-shrink-0 mr-5 mb-4">
+          <div className="group hover:bg-gray-100 border border-gray-400 text-gray-400 px-4 py-2 rounded-xl flex items-center cursor-pointer h-full">
+            <button type="button" className="flex">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 group-hover:bg-gray-100"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"
+                />
+              </svg>
+              Delivery Time
+            </button>
+          </div>
+        </div>
+        <div className="flex-shrink-0 mr-5 mb-4">
+          <div className="group hover:bg-gray-100 border border-gray-400 text-gray-400 px-4 py-2 rounded-xl flex items-center cursor-pointer h-full">
+            <button type="button" className="flex">
+              Great Offers
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* <div className="flex justify-start items-center mx-40 my-3">
+        <div className="flex flex-wrap">
+          <button
+            type="button"
+            className="flex bg-white hover:bg-gray-100 text-gray-400 px-6 py-2 rounded font-medium mx-3 border border-gray-300 transition duration-200 each-in-out"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 hover:bg-gray-100"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+              />
+            </svg>
+            Filters
+          </button>
+          <button
+            type="button"
+            className="flex bg-white hover:bg-gray-100 text-gray-400 px-6 py-2 rounded font-medium mx-3 border border-gray-300 transition duration-200 each-in-out"
+          >
+            Rating: 4.0+
+          </button>
+          <button
+            type="button"
+            className="flex bg-white hover:bg-gray-100 text-gray-400 px-6 py-2 rounded font-medium mx-3 border border-gray-300 transition duration-200 each-in-out"
+          >
+            Safe and Hygenic
+          </button>
+          <button
+            type="button"
+            className="flex bg-white hover:bg-gray-100 text-gray-400 px-6 py-2 rounded font-medium mx-3 border border-gray-300 transition duration-200 each-in-out"
+          >
+            Pure Veg
+          </button>
+          <button
+            type="button"
+            className="flex bg-white hover:bg-gray-100 text-gray-400 px-6 py-2 rounded font-medium mx-3 border border-gray-300 transition duration-200 each-in-out"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 hover:bg-gray-100"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"
+              />
+            </svg>
+            Delivery Time
+          </button>
+          <button
+            type="button"
+            className="flex bg-white hover:bg-gray-100 text-gray-400 px-6 py-2 rounded font-medium mx-3 border border-gray-300 transition duration-200 each-in-out"
+          >
+            Great Offers
+          </button>
+        </div>
+      </div> */}
+    </div>
+  );
+};
 
-export default FilterButtons
+export default FilterButtons;
