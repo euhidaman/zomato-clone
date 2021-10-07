b75fdaaf4d88b4e7fa08e6bc97b79248f6ce4dd7
Euhid Aman <aman.derax20@gmail.com>
2021-10-07 19:22:03 +0530

    Changed left margin for coloredLine

--- src/components/ColoredLine.js
+++ src/components/ColoredLine.js
@@ -4,7 +4,7 @@ const ColoredLine = ({ color }) => (
     <div className="py-2">
         <hr
             style={{
-                marginLeft: 165,
+                marginLeft: 185,
                 color: "red",
                 backgroundColor: "red",
                 width: 200,
